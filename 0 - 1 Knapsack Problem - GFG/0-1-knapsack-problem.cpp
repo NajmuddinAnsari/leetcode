25e757326732b3edb576b367fd5fe918d5396862
TypeCoder <88725168+NajmuddinAnsari@users.noreply.github.com>
2022-12-31 19:49:12 +0530

    Added solution - LeetHub

---
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to return max value that can be put in knapsack of capacity W.
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       // Your code here
+    //   if(n==0 || W==0){
+    //       return 0;
+    //   }
+    //   if(wt[n-1]<=W){
+    //       return max(knapSack(W-wt[n-1],wt,val,n-1)+val[n-1],knapSack(W,wt,val,n-1));
+    //   }
+    //   else{
+    //       return knapSack(W,wt,val,n-1);
+    //   }
+       int dp[n+1][W+1];
+       for(int i=0;i<n+1;i++){
+            for(int j=0;j<W+1;j++){
+                if(i==0||j==0){
+                    dp[i][j]=0;
+                }
+                else{
+                    dp[i][j]=-1;
+                }
+            }
+       }
+    //   for(int i=0;i<n+1;i++){
+    //         for(int j=0;j<W+1;j++){
+    //             cout<<dp[i][j]<<" ";
+    //         }
+    //         cout<<endl;
+    //   }
+       for(int i=1;i<n+1;i++){
+           for(int j=1;j<W+1;j++){
+               if(wt[i-1]<=j){
+                   dp[i][j]=max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
+               }
+               else{
+                   dp[i][j]=dp[i-1][j];
+               }
+           }
+       }
+       return dp[n][W];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+ {
+    //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //reading number of elements and weight
+        int n, w;
+        cin>>n>>w;
+        
+        int val[n];
+        int wt[n];
+        
+        //inserting the values
+        for(int i=0;i<n;i++)
+            cin>>val[i];
+        
+        //inserting the weights
+        for(int i=0;i<n;i++)
+            cin>>wt[i];
+        Solution ob;
+        //calling method knapSack()
+        cout<<ob.knapSack(w, wt, val, n)<<endl;
+        
+    }
+	return 0;
+}
+// } Driver Code Ends
