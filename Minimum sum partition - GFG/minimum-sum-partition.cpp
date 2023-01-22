5f25fa9e011809d848080dd61f71e15f9da2bf92
TypeCoder <88725168+NajmuddinAnsari@users.noreply.github.com>
2023-01-23 01:34:35 +0530

    Added solution - LeetHub

---
+++ Minimum sum partition - GFG/minimum-sum-partition.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+
+  public:
+    void subsetSum(int arr[],int sum,int n,vector<int> &possibleS1){
+        int dp[n+1][sum+1];
+        for(int i=0;i<n+1;i++){
+            dp[i][0]=true;
+        }
+        for(int i=1;i<sum+1;i++){
+            dp[0][i]=false;
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<sum+1;j++){
+                if(arr[i-1]<=j){
+                    dp[i][j]=dp[i-1][j-arr[i-1]] || dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        for(int i=0;i<sum+1;i++){
+            if(dp[n][i]){
+                possibleS1.push_back(i);
+            }
+        }
+    }
+	int minDifference(int arr[], int n)  { 
+        vector<int> possibleS1;
+        int sum=accumulate(arr,arr+n,0);
+        subsetSum(arr,sum/2,n,possibleS1);
+        // for(int i=0;i<possibleS1.size();i++){
+        //     cout<<possibleS1[i]<<" ";
+        // }
+        // cout<<endl;
+        int ans=INT_MAX;
+        for(int i=0;i<possibleS1.size();i++){
+            ans=min(ans,sum-2*possibleS1[i]);
+        }
+        return ans;
+	} 
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{
+   
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int a[n];
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+       
+
+	    Solution ob;
+	    cout << ob.minDifference(a, n) << "\n";
+	     
+    }
+    return 0;
+}
+// } Driver Code Ends
