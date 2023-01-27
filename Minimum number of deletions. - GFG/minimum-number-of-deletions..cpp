c01baff22b1550eeaf65342e983623f044a30075
TypeCoder <88725168+NajmuddinAnsari@users.noreply.github.com>
2023-01-27 23:38:47 +0530

    Added solution - LeetHub

---
+++ Minimum number of deletions. - GFG/minimum-number-of-deletions..cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+int minDeletions(string str, int n);
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        cout << minDeletions(s, n) << endl;
+
+    }
+return 0;
+}
+// } Driver Code Ends
+
+
+int longestCommonSubsequence(string text1, string text2) {
+        int n=text1.size();
+        int m=text2.size();
+        int dp[n+1][m+1];
+        for(int i=0;i<n+1;i++){
+            dp[i][0]=0;
+        }
+        for(int i=0;i<m+1;i++){
+            dp[0][i]=0;
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(text1[i-1]==text2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+int minDeletions(string str, int n) { 
+    string str2="";
+    for(int i=n-1;i>=0;i--){
+        str2+=str[i];
+    }
+    return n-longestCommonSubsequence(str,str2);
+} 
